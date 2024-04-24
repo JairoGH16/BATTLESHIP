ac5d1f2d528d8c2cbcc18600d3a5728bfdbb526d
JairoGH16 <162930229+JairoGH16@users.noreply.github.com>
2024-04-23 20:36:06 -0600

    Update vidas.py

--- logica/vidas.py
+++ logica/vidas.py
@@ -1,14 +1,24 @@
 import tkinter as tk
+puntuacion_j1=0
+puntuacion_j2=0
 nombre_j1=""
 nombre_j2=""
-ultimo_intento=(False,False)
+ultimo_intento=[False,False]
 
 vidas_jugador1={"barcos_pequenos":6,
+<<<<<<< HEAD
+                "barcos_medianos":4,
+                "barcos_grandes":2}
+vidas_jugador2={"barcos_pequenos":6,
+                "barcos_medianos":4,
+                "barcos_grandes":2}
+=======
                 "barcos_medianos":8,
                 "barcos_grandes":6}
 vidas_jugador2={"barcos_pequenos":6,
                 "barcos_medianos":8,
                 "barcos_grandes":6}
+>>>>>>> d7588d448610c65d502f98bde47de94a500209ce
 
 def quitar_vidas(jugador:int,tipo_barco:str):
     global ultimo_intento
@@ -20,11 +30,13 @@ def quitar_vidas(jugador:int,tipo_barco:str):
     if vidas_jugador1["barcos_pequenos"]==0 and vidas_jugador1["barcos_medianos"]==0 and vidas_jugador1["barcos_grandes"]==0:
         if vidas_jugador2["barcos_pequenos"]==0 and vidas_jugador2["barcos_medianos"]==0 and vidas_jugador2["barcos_grandes"]==0:
             empate()
+            return
     #Gana jugador 2
     #si una es cero y la otra mayor a 1 gana la primera
     if vidas_jugador1["barcos_pequenos"]==0 and vidas_jugador1["barcos_medianos"]==0 and vidas_jugador1["barcos_grandes"]==0:
         if vidas_jugador2["barcos_pequenos"]+vidas_jugador2["barcos_medianos"]+vidas_jugador2["barcos_grandes"]>1:
             anunciar_ganador(nombre_j2)
+            return
     #si uno es cero y la otra es 1 el turno continua
     if vidas_jugador1["barcos_pequenos"]==0 and vidas_jugador1["barcos_medianos"]==0 and vidas_jugador1["barcos_grandes"]==0:
         if vidas_jugador2["barcos_pequenos"]+vidas_jugador2["barcos_medianos"]+vidas_jugador2["barcos_grandes"]==1:
@@ -33,6 +45,7 @@ def quitar_vidas(jugador:int,tipo_barco:str):
                 ultimo_intento[0]=True
             else:
                 anunciar_ganador(nombre_j2)
+                return
     #Gana jugador 1
     #si una es cero y la otra mayor a 1 gana la primera
     if vidas_jugador2["barcos_pequenos"]==0 and vidas_jugador2["barcos_medianos"]==0 and vidas_jugador2["barcos_grandes"]==0:
@@ -46,6 +59,16 @@ def quitar_vidas(jugador:int,tipo_barco:str):
                 ultimo_intento[1]=True
             else:
                 anunciar_ganador(nombre_j1)
+                return
+
+def perder_empate(jugador:int,mar:list,pos_y:int,pos_x:int):
+    if ultimo_intento[jugador]==True and mar[pos_y][pos_x]["pieza"]==".":
+        if jugador==0:
+            anunciar_ganador(nombre_j2)
+            return
+        if jugador==1:
+            anunciar_ganador(nombre_j1)
+            return
 
 def anunciar_ganador(jugador):
     ventana = tk.Tk()
